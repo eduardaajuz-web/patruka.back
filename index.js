682fdf001398ab58a7e52ce8a8f90150b93cd249
duda 🕸️˙⟡ <eduarda.ajuz@escola.pr.gov.br>
2026-08-18 12:19:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function atualizarID() {
 app.post("/aulas", (req, res) => {
     const aula = req.body
     try {
-            const bd = JSON.parse(fs.readFileSync("aulas.json", "utf8"))
-            const produtos = JSON.parse(fs.readFileSync("produtos.json", "utf8"))
+            
+            const aulas = JSON.parse(fs.readFileSync("aulas.json", "utf8"))
             atualizarID()
-            produto.id = id
-        bd.push(aula)
-         fs.writeFileSync("aulas.json", JSON.stringify(bd), "utf8")
+            aula.id = id
+        aulas.push(aula)
+         fs.writeFileSync("aulas.json", JSON.stringify(aulas), "utf8")
         res.status(201).json({resposta: "Aula cadastrada!"})
     } catch (erro) {
         res.status(500).json({erro: erro.message})
